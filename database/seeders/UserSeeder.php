642b2ae378c71e23fcc8f85c3fdb7391db59eb20
Nurazizahs <nurazizahsabilillah@gmail.com>
2023-10-14 15:00:30 +0700

    List User, Pagination, Search

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //User::factory(5)->create();
+        User::factory(5)->create();
          User::create([
              'name'=>'sabil',
              'email'=>'sabil@gmail.com',
